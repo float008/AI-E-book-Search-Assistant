f7224b16a8fd8866bd2545a27f9d563ee474a67b
ruanqi <347282587@qq.com>
2026-05-26 23:14:35 +0800

    feat: vercel ai sdk

--- nest-langchain/src/ai/ai.controller.ts
+++ nest-langchain/src/ai/ai.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get, MessageEvent, Query, Sse } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  MessageEvent,
+  Post,
+  Query,
+  Res,
+  Sse,
+} from '@nestjs/common';
 import { AiService } from './ai.service';
 import { Observable, from } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+import { pipeUIMessageStreamToResponse, UIMessage } from 'ai';
+import type { Response } from 'express';
 @Controller('ai')
 export class AiController {
   constructor(private readonly aiService: AiService) {}
@@ -22,5 +33,18 @@ export class AiController {
         data: chunk,
       })),
     );
+  }
+
+  @Post('chat')
+  async postChat(
+    @Body() body: { messages?: UIMessage[] },
+    @Res({ passthrough: false }) res: Response,
+  ): Promise<void> {
+    if (!body?.messages || !Array.isArray(body.messages)) {
+      throw new BadRequestException('Invalid JSON');
+    }
+
+    const stream = await this.aiService.stream(body.messages);
+    pipeUIMessageStreamToResponse({ response: res, stream });
   }
 }
